2df428761d195187668d17723acb2627104b443a
teppokoivula <teppo.koivula@gmail.com>
2019-06-18 22:51:16 +0300

    Switch MarkupSimpleNavigation to MarkupMenu

--- templates/views/partials/menu/mobile.php
+++ templates/views/partials/menu/mobile.php
@@ -3,4 +3,12 @@
 <input type="checkbox" id="mobile-menu-toggle" class="menu-toggle" hidden>
 <label for="mobile-menu-toggle" class="menu-toggle-label"><i class="fas fa-bars"></i></label>
 
-<?= renderMenu('Navigation', 'menu_class_modifier=mobile', null, $home) ?>
+<?php
+echo $menu->render([
+    'root_page' => $home,
+    'current_page' => $page,
+    'placeholders' => [
+        'menu_class_modifier' => 'mobile',
+        'aria_label' => 'Navigation',
+    ],
+]);
